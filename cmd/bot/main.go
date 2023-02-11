d09cedef27b476987a8882d491c442bc2417e253
Anastasiya Muravieva <anastasiya.muravieva@timebook.ru>
2023-02-11 21:47:04 +0300

    First iteration

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+const token = "TOKEN"
 
 func main() {
-	fmt.Println("bot")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+			//switch update.Message.Command() {
+			//case "help":
+			//	msg.Text = "type /sayhi or /status."
+			//case "sayhi":
+			//	msg.Text = "Hi :)"
+			//case "status":
+			//	msg.Text = "I'm ok."
+			//case "withArgument":
+			//	msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+			//case "html":
+			//	msg.ParseMode = "html"
+			//	msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+			//default:
+			//	msg.Text = "I don't know that command"
+			//}
+			bot.Send(msg)
+		}
+
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Muravjeva/route_256_t_me_bot
 
 go 1.20
+
+require github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+
+require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
