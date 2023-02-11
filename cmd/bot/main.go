33d4cd18b47f5fa0344b1074c28cfc502dea7851
Anastasiya Muravieva <anastasiya.muravieva@timebook.ru>
2023-02-12 00:05:45 +0300

    Added HandleUpdate method

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -10,11 +10,6 @@ import (
 )
 
 func main() {
-	defer func() {
-		if panicValue := recover(); panicValue != nil {
-			log.Printf("recovered from panic: %v", panicValue)
-		}
-	}()
 	godotenv.Load()
 	token := os.Getenv("TOKEN")
 	bot, err := tgbotapi.NewBotAPI(token)
@@ -40,38 +35,6 @@ func main() {
 	commander := commands.NewCommander(bot, productService)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		if update.Message.IsCommand() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "help":
-				commander.Help(update.Message)
-			case "sayhi":
-				msg.Text = "Hi :)"
-			case "status":
-				msg.Text = "I'm ok."
-			case "withArgument":
-				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
-			case "html":
-				msg.ParseMode = "html"
-				msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
-			case "list":
-				commander.List(update.Message)
-			case "get":
-				commander.Get(update.Message)
-			default:
-				commander.Default(update.Message)
-			}
-
-			bot.Send(msg)
-			continue
-		}
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
-		bot.Send(msg)
+		commander.HandleUpdate(update)
 	}
 }
--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"github.com/Muravjeva/route_256_t_me_bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
 )
 
 type Commander struct {
@@ -15,5 +16,40 @@ func NewCommander(bot *tgbotapi.BotAPI,
 	return &Commander{
 		bot:            bot,
 		productService: productService,
+	}
+}
+
+func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	defer func() {
+		if panicValue := recover(); panicValue != nil {
+			log.Printf("recovered from panic: %v", panicValue)
+		}
+	}()
+
+	if update.Message == nil {
+		return
+	}
+
+	if update.Message.IsCommand() {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "help":
+			c.Help(update.Message)
+		case "sayhi":
+			msg.Text = "Hi :)"
+		case "status":
+			msg.Text = "I'm ok."
+		case "withArgument":
+			msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+		case "html":
+			msg.ParseMode = "html"
+			msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+		case "list":
+			c.List(update.Message)
+		case "get":
+			c.Get(update.Message)
+		default:
+			c.Default(update.Message)
+		}
 	}
 }
